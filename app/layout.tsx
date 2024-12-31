832aad49b3ff610f0ec6753a6eb85d0b0ee4c677
Justice <sherllyj191@gmail.com>
2024-12-31 13:28:41 +0100

    Updated favicon | added more favicon icons, options

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,31 +1,32 @@
-import type { Metadata } from "next";
-import { Manrope } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from "next"
+import { Manrope } from "next/font/google"
+import "./globals.css"
 
 const manrope = Manrope({
   variable: "--font-manrope",
   subsets: ["latin"],
   weight: ["400", "500", "700"],
-});
+})
 
-const site = process.env.NEXT_PUBLIC_SITE_URL || "";
+const site = process.env.NEXT_PUBLIC_SITE_URL || ""
 // const site_without_protocol = site.replace(/^https?:\/\//, "");
 
 const meta = {
   title: "Audiophile E-commerce",
   name: "Audiophile",
   description: "Experience natural, lifelike audio and exceptional build quality made for the passionate music enthusiast. 🎧🔊",
-};
+}
 
 const images = {
   banner: `/banner.png`,
-};
+}
 
 export const metadata: Metadata = {
   title: meta.title,
   description: meta.description,
   applicationName: meta.name,
   creator: "Justice N.",
+  manifest: "/site.webmanifest",
   keywords: ["audiophile", "music", "audio", "headphones", "earphones", "speakers", "next.js", "react", "typescript"],
   openGraph: {
     type: "website",
@@ -33,19 +34,25 @@ export const metadata: Metadata = {
     title: meta.title,
     description: meta.description,
     siteName: meta.name,
-    images: [{ url: images.banner }],
+    images: [{ url: images.banner }, { url: "/favicon.ico" }, { url: "/favicon-32x32.png" }, { url: "/favicon-16x16.png" }],
   },
   twitter: { card: "summary_large_image", site: "@site", creator: "@creator", images: images.banner },
-};
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="en">
-      <body className={`${manrope.variable}  antialiased`}>{children}</body>
+      <head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        {/* <link rel="manifest" href="/site.webmanifest" /> */}
+      </head>
+      <body className={`${manrope.variable} antialiased`}>{children}</body>
     </html>
-  );
+  )
 }
