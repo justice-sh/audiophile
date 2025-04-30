32c7194a150283f9a5bf53745b237c3e18e88c3b
Justice <sherllyj191@gmail.com>
2025-04-30 12:51:27 +0100

    Changes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,5 @@
 import type { Config } from "tailwindcss";
-import plugin from "tailwindcss/plugin";
-// import { typography } from "./tailwind-config/typography";
-// import { breakpoints } from "./tailwind-config/breakpoints";
-// import { colors } from "./tailwind-config/colors";
+
 
 export default {
   darkMode: ["class"],
