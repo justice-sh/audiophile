ec7b8765487335e3695fcd12a00e6534b1f8ee9e
Justice <sherllyj191@gmail.com>
2025-05-18 17:33:28 +0100

    Fixed build error

--- app/checkout/ui/summary/ui/cart-item.tsx
+++ app/checkout/ui/summary/ui/cart-item.tsx
@@ -1,6 +1,4 @@
 import { CartItemLayout } from "@/shared/components/cart-item-layout"
-import { Counter } from "@/shared/components/counter"
-import { cartStore } from "@/shared/data/cart-store"
 import { CartItem as CartItemData } from "@/shared/types/cart"
 
 type Props = {
@@ -8,10 +6,6 @@ type Props = {
 }
 
 export function CartItem({ item }: Props) {
-  const handleCounterChange = (value: number) => {
-    cartStore.trigger.set({ ...item, quantity: value })
-  }
-
   return (
     <CartItemLayout item={item} className="flex items-center gap-4">
       <p className="font-bold text-black/50">x{item.quantity}</p>
